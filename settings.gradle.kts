2ae1648624691170f32d7c52d19a93bfa18b6987
nurulapriliaaa <nurulapriliaaa@gmail.com>
2023-10-18 01:51:41 -0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Percobaan4_F55122010"
+include(":app")
